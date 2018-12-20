9fb88a56b7f4f3f7df66bef0600ecc93a2d6f26f
7mioda <ahmededaly1993@gmail.com>
2018-12-20 09:35:16 +0100

    OT8 ListCard Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Header from './Components/Header/Header';
 import EmployeeList from './Components/EmployeeList/EmployeeList';
 
 
 const App = () => (
   <div>
+    <Header />
     <EmployeeList />
   </div>
 );
